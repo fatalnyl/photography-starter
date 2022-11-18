88c60921678f8e8d75db3251f5aaff20fb4562ef
fatalnyl <fatalnylism@gmail.com>
2022-11-18 08:36:01 +0800

    another 1

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState } from "react"
+// import icons
+import { IoMdClose } from "react-icons/io"
+import { CgMenuRight } from "react-icons/cg"
+// import Link
+import { Link } from "react-router-dom"
+// import motion
+import { motion } from "framer-motion"
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
-};
+  return (
+    <nav>
+      {/* nav open button */}
+      <div className="text-3x1 cursor-pointer">
+        <CgMenuRight />
+      </div>
 
-export default MobileNav;
+      {/*   top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%); */}
+      {/* menu */}
+      <div className="bg-white shadow-2x1 w-full absolute top-0 right-0 max-w-xs h-screen">
+        {/* icon */}
+        <div className="text-3xl flex absolute z-30 left-4 top-20 text-primary cursor-pointer">
+          <IoMdClose />
+        </div>
+        <ul className="h-full  flex flex-col justify-center items-center gap-y-8 text-primary font-primary font-bold text-3xl">
+          <li>
+            <Link to={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link to={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link to={"/portfolio"}>Portfolio</Link>
+          </li>
+          <li>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default MobileNav
