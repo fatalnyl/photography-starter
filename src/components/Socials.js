bfd455be22421287d552a5f47f2705cb23ed661c
fatalnyl <fatalnylism@gmail.com>
2022-11-30 04:27:51 +0800

    first

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,9 +1,14 @@
-import React from "react"
+import React, {useContext} from "react"
 // import icons
 import { ImFacebook, ImTwitter, ImPinterest, ImInstagram, ImYoutube } from "react-icons/im"
+import { CursorContext } from "../context/CursorContext"
+
 const Socials = () => {
+
+  const { mouseEnterHandler, mouseLeaveHandler } = useContext(CursorContext)
+
   return (
-    <div className="hidden xl:flex ml-24">
+    <div onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}className="hidden xl:flex ml-24">
       <ul className="flex gap-x-4">
         <li>
           <a href="http://www.facebook.com" target="_blank">
